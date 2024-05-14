e65978f8c0a1ee41c34c27526f9a4228c8c756e3
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2024-05-14 22:31:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://arya-aha-308f073a0e00.herokuapp.com/")
+URL = environ.get("URL", "https://aryamain-778d89638d7e.herokuapp.com/")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
